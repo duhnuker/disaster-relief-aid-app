bb05170d01704d855a88be0076d4cdfc2e0c27e6
duhnuker <dinukanh@gmail.com>
2024-08-01 20:54:12 +1000

    login route

--- server/routes/jwtAuth.ts
+++ server/routes/jwtAuth.ts
@@ -37,4 +37,32 @@ router.post("/register", async (req: Request, res: Response) => {
     }
 })
 
+router.post("/login", async (req, res) => {
+    try {
+       const { email, password } = req.body;
+
+       const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+       if (user.rows.length === 0) {
+        return res.status(401).json("Password or Email is incorrect");
+       }
+
+       //Check if incoming password is correct
+       const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+       if(!validPassword) {
+        return res.status(401).json("Password or Email is incorrect");
+       }
+
+       //Give them the JWT token
+       const token = jwtGenerator(user.rows[0].user_id);
+       res.json({ token });
+        
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Server Error");
+    }
+})
+
 module.exports = router;
