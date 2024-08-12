32ac3a219ef635474f6e6737729eb7a1c51f1525
duhnuker <dinukanh@gmail.com>
2024-08-12 20:48:01 +1000

    Basic Login component styling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -31,10 +31,10 @@ const Login = ({ setAuth }) => {
           }
         }
       );
-  
-          localStorage.setItem("token", response.data.jwtToken);
-          setAuth(true);
-          toast.success("Logged in successfully!");
+
+      localStorage.setItem("token", response.data.jwtToken);
+      setAuth(true);
+      toast.success("Logged in successfully!");
 
     } catch (err) {
       console.error(err.message);
@@ -42,26 +42,36 @@ const Login = ({ setAuth }) => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={onSubmitForm}>
-        <input
-          type='email'
-          name='email'
-          placeholder='email'
-          value={email}
-          onChange={e => onChange(e)}>
-        </input>
-        <input
-          type='password'
-          name='password'
-          placeholder='password'
-          value={password}
-          onChange={e => onChange(e)}>
-        </input>
-        <button>Login</button>
-      </form>
-      <Link to="/register">Register</Link>
+    <div className='grid grid-cols-3 grid-rows-3 h-screen w-screen place-items-center bg-red-900'>
+      <div className='flex flex-col items-center justify-center border-4 border-red-800 rounded-3xl shadow-2xl col-start-2 row-start-2 p-5 min-h-full min-w-full'>
+      <img className='w-32 h-32 rounded-full my-5' src='public/logo.jpeg' alt='SafeHaven Logo'></img>
+        <h1 className='text-5xl font-bold pb-8 text-white text-center'>Welcome back,</h1>
+        <form onSubmit={onSubmitForm}>
+          <input className='border-2 border-gray-900 mb-2 font-bold text-lg pl-2 rounded-md'
+            type='email'
+            name='email'
+            placeholder='Email'
+            value={email}
+            onChange={e => onChange(e)}>
+          </input>
+          <br></br>
+          <input className='border-2 border-gray-900 mb-4 font-bold text-lg pl-2 rounded-md'
+            type='password'
+            name='password'
+            placeholder='Password'
+            value={password}
+            onChange={e => onChange(e)}>
+          </input>
+          <br></br>
+          <div className="text-center">
+            <button className='bg-gray-800 rounded-md text-white px-2 py-1 font-bold mb-10 text-md hover:bg-gray-700 duration-300'>Login</button>
+          </div>
+        </form>
+        <p className='text-white text-md pb-2'>Don't have an account?</p>
+        <div className='mb-5'>
+          <Link to="/register" className='bg-gray-800 rounded-md text-white px-2 py-1 text-md hover:bg-gray-700 duration-300'>Register here!</Link>
+        </div>
+      </div>
     </div>
   )
 }
