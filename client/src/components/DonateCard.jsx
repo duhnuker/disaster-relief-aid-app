3136902b03deab5b642800322886f478149b909c
duhnuker <dinukanh@gmail.com>
2024-08-15 23:43:09 +1000

    DonateCard component created

---
+++ client/src/components/DonateCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DonateCard = (props) => {
+  return (
+    <div className='p-4 rounded-3xl shadow-2xl text-white font-bold'>
+        <h1 className='pb-4 text-3xl'>{props.name}</h1>
+        <img src={props.img} className=' rounded-lg w-96' />
+        <p className='pt-4'>{props.description}</p>
+        <div>
+          <button className='bg-gray-800 rounded-md px-2 py-1 mb-10 text-md hover:bg-gray-700 duration-300 mt-5'>Donate Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default DonateCard
